2381b8c8e470d23d6862963c1ff34f097c9c3490
weixingsun <weixing.sun@gmail.com>
2017-10-27 00:47:07 +0800

    autoBalance()

--- drone_9250.ino
+++ drone_9250.ino
@@ -17,109 +17,16 @@ SoftwareSerial BLE_Serial(2, 3); // BLE's RX, BLE's TXD
 MPU9250 myIMU;
 Servo escs[4];
 int avg_speed,new_speed,tmp_speed,tmp_speed_total,i;
-
 //////////////////////////////////////////////////////////////////////////////////
-#define RESTRICT_PITCH // Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
-Kalman kalmanX; // Create the Kalman instances
+// Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
+#define RESTRICT_PITCH
+Kalman kalmanX;
 Kalman kalmanY;
-double gyroXangle, gyroYangle; // Angle calculate using the gyro only
-double compAngleX, compAngleY; // Calculated angle using a complementary filter
-double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+int RIGHT_REAR=0, RIGHT_FRONT=1, LEFT_FRONT=2, LEFT_REAR=3;
 uint32_t timer;
 double delta;
-//////////////////////////////////////////////////////////////////////////////////
-bool cmd_end=false;
 String cmd="";
-//////////////////////////////////////////////////////////////////////////////////
-/*
-void initKalman(){
-  // Set kalman and gyro starting angle
-  while (i2cRead(0x3B, i2cData, 6));
-  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
-  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
-  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
-
-  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
-  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
-  // It is then converted from radians to degrees
-#ifdef RESTRICT_PITCH // Eq. 25 and 26
-  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
-  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
-#else // Eq. 28 and 29
-  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
-  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
-#endif
-  kalmanX.setAngle(roll); // Set starting angle
-  kalmanY.setAngle(pitch);
-  gyroXangle = roll;
-  gyroYangle = pitch;
-  compAngleX = roll;
-  compAngleY = pitch;
-  timer = micros();
-}
-*/
-void computeKalman(){
-  delta = (double)(micros() - timer) / 1000000; // Calculate delta time
-  timer = micros();
-  double gyroXrate = myIMU.gx / 131.0; // Convert to deg/s
-  double gyroYrate = myIMU.gy / 131.0; // Convert to deg/s
-
-#ifdef RESTRICT_PITCH
-  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
-  if ((myIMU.roll < -90 && kalAngleX > 90) || (myIMU.roll > 90 && kalAngleX < -90)) {
-    kalmanX.setAngle(myIMU.roll);
-    compAngleX = myIMU.roll;
-    kalAngleX  = myIMU.roll;
-    gyroXangle = myIMU.roll;
-  } else
-    kalAngleX = kalmanX.getAngle(myIMU.roll, gyroXrate, delta); // Calculate the angle using a Kalman filter
-
-  if (abs(kalAngleX) > 90)
-    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
-  kalAngleY = kalmanY.getAngle(myIMU.pitch, gyroYrate, delta);
-#else
-  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
-  if ((myIMU.pitch < -90 && kalAngleY > 90) || (myIMU.pitch > 90 && kalAngleY < -90)) {
-    kalmanY.setAngle(myIMU.pitch);
-    compAngleY = myIMU.pitch;
-    kalAngleY = myIMU.pitch;
-    gyroYangle = myIMU.pitch;
-  } else
-    kalAngleY = kalmanY.getAngle(myIMU.pitch, gyroYrate, delta); // Calculate the angle using a Kalman filter
-
-  if (abs(kalAngleY) > 90)
-    gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
-  kalAngleX = kalmanX.getAngle(myIMU.roll, gyroXrate, delta); // Calculate the angle using a Kalman filter
-#endif
-
-  gyroXangle += gyroXrate * delta; // Calculate gyro angle without any filter
-  gyroYangle += gyroYrate * delta;
-  //gyroXangle += kalmanX.getRate() * delta; // Calculate gyro angle using the unbiased rate
-  //gyroYangle += kalmanY.getRate() * delta;
-
-  compAngleX = 0.93 * (compAngleX + gyroXrate * delta) + 0.07 * myIMU.roll; // Calculate the angle using a Complimentary filter
-  compAngleY = 0.93 * (compAngleY + gyroYrate * delta) + 0.07 * myIMU.pitch;
-
-  // Reset the gyro angle when it has drifted too much
-  if (gyroXangle < -180 || gyroXangle > 180)
-    gyroXangle = kalAngleX;
-  if (gyroYangle < -180 || gyroYangle > 180)
-    gyroYangle = kalAngleY;
-
-}
-void printKalman(){
-  Serial.print("roll="); Serial.print(myIMU.roll); Serial.print("\t");
-  Serial.print("  gx=");Serial.print(myIMU.gx); Serial.print("\t");
-  Serial.print("gxagl=");Serial.print(gyroXangle); Serial.print("\t");
-  Serial.print("caglx=");Serial.print(compAngleX); Serial.print("\t");
-  Serial.print("kaglx=");Serial.print(kalAngleX); Serial.print("\t");
-  Serial.print("pitch=");Serial.print(myIMU.pitch); Serial.print("\t");
-  Serial.print("  gy=");Serial.print(myIMU.gy); Serial.print("\t");
-  Serial.print("gyagl=");Serial.print(gyroYangle); Serial.print("\t");
-  Serial.print("cagly=");Serial.print(compAngleY); Serial.print("\t");
-  Serial.print("kagly=");Serial.print(kalAngleY); Serial.println("\t");
-
-}
+bool cmd_end=false;
 //////////////////////////////////////////////////////////////////////////////////
 void setup() {
   Wire.begin();
@@ -133,7 +40,7 @@ void loop() {
   readCmd();
   getDataMPU9250();
   //printMPU9250();
-  computeKalman();
+  //computeKalman();
   //printKalman();
   autoBalance();
   delay(200);
@@ -256,19 +163,31 @@ void getDataMPU9250(){
 void autoBalance(){
     Serial.print("myIMU.roll=");Serial.print(myIMU.roll);Serial.print('\t');
     Serial.print("myIMU.pitch=");Serial.println(myIMU.pitch);
-    if(myIMU.roll>0.1 && myIMU.gx>0.1){
-       changeASpeed(0,1);
-       Serial.println("inc esc 0 ");
-    }else if(myIMU.roll>0.1 && myIMU.gx>0.1) {
-       changeASpeed(0,-1);
-       Serial.println("dec esc 0 ");
+    if(myIMU.roll<-5 && myIMU.gx<-1){             //left down
+       changeASpeed(LEFT_FRONT,1);
+       changeASpeed(LEFT_REAR, 1);
+       changeASpeed(RIGHT_FRONT,-1);
+       changeASpeed(RIGHT_REAR, -1);
+       Serial.println("inc esc left, dec esc right ");
+    }else if(myIMU.roll>5 && myIMU.gx>1) {        //left up
+       changeASpeed(LEFT_FRONT,-1);
+       changeASpeed(LEFT_REAR, -1);
+       changeASpeed(RIGHT_FRONT,1);
+       changeASpeed(RIGHT_REAR, 1);
+       Serial.println("dec esc left, inc esc right ");
     }
-    if(myIMU.roll>0.1 && myIMU.gx>0.1) {
-       changeASpeed(3,1);
-       Serial.println("inc esc 3 ");
-    }else if(myIMU.roll>0.1 && myIMU.gx>0.1) {
-       changeASpeed(3,-1);
-       Serial.println("dec esc 3 ");
+    if(myIMU.pitch>5 && myIMU.gy>1) {            //head down
+       changeASpeed(LEFT_FRONT, 1);
+       changeASpeed(RIGHT_FRONT,1);
+       changeASpeed(LEFT_REAR, -1);
+       changeASpeed(RIGHT_REAR,-1);
+       Serial.println("dec esc rear, inc esc head ");
+    }else if(myIMU.roll<-5 && myIMU.gx<-1) {     //head up
+       changeASpeed(LEFT_FRONT, -1);
+       changeASpeed(RIGHT_FRONT,-1);
+       changeASpeed(LEFT_REAR,   1);
+       changeASpeed(RIGHT_REAR,  1);
+       Serial.println("inc esc rear, dec esc head ");
     }
 }
 void printMPU9250(){
@@ -323,3 +242,30 @@ int Error(int code, String msg){
   Serial.flush();
   abort();
 }
+/*
+void initKalman(){
+  // Set kalman and gyro starting angle
+  while (i2cRead(0x3B, i2cData, 6));
+  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
+  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
+  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+  kalmanX.setAngle(roll); // Set starting angle
+  kalmanY.setAngle(pitch);
+  gyroXangle = roll;
+  gyroYangle = pitch;
+  compAngleX = roll;
+  compAngleY = pitch;
+  timer = micros();
+}
+*/
