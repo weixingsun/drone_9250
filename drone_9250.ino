a312767d53d9c3490733e78e4637e3f2650d0450
U-LENOVO\sunwx6 <sunwx6@SUNWX6-R303HPVR.lenovo.com>
2017-10-29 01:21:20 +0800

    add mpu6050

--- drone_9250.ino
+++ drone_9250.ino
@@ -1,23 +1,28 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 #include "Kalman.h"
-#include "MPU9250.h"
+//#define MPU_9250
 #define ArraySize(x) (sizeof(x) / sizeof(x[0]))
 //String toStr(int i) {return String(i);}
 //////////////////////////////////////////////////////////////////////////////////
 const int WHO_AM_I = 0x73;
 const int esc_pins[4] = {5,9,10,11};
-const int minPulseRate        = 1000;//700
-const int maxPulseRate        = 2000;
+const int minPulseRate = 1000;//700
+const int maxPulseRate = 2000;
 const int numSensorDataSize = 10;
 const long BT_RATE = 115200;
 const int  CMD_LEN = 10;
 //////////////////////////////////////////////////////////////////////////////////
 SoftwareSerial BLE_Serial(2, 3); // BLE's RX, BLE's TXD
-MPU9250 myIMU;
+#ifdef MPU_9250
+  #include "MPU9250.h"
+  MPU9250 myIMU;
+#else
+  #include "MPU6050.h"
+  MPU6050 myIMU;
+#endif
 Servo escs[4];
-int avg_speed,new_speed,tmp_speed,tmp_speed_total,i;
-
+int avg_speed,tmp_speed,tmp_speed_total,i;
 //////////////////////////////////////////////////////////////////////////////////
 #define RESTRICT_PITCH // Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
 Kalman kalmanX; // Create the Kalman instances
@@ -30,112 +35,35 @@ double delta;
 //////////////////////////////////////////////////////////////////////////////////
 bool cmd_end=false;
 String cmd="";
-//////////////////////////////////////////////////////////////////////////////////
-/*
-void initKalman(){
-  // Set kalman and gyro starting angle
-  while (i2cRead(0x3B, i2cData, 6));
-  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
-  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
-  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
 
-  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
-  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
-  // It is then converted from radians to degrees
-#ifdef RESTRICT_PITCH // Eq. 25 and 26
-  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
-  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
-#else // Eq. 28 and 29
-  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
-  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
-#endif
-  kalmanX.setAngle(roll); // Set starting angle
-  kalmanY.setAngle(pitch);
-  gyroXangle = roll;
-  gyroYangle = pitch;
-  compAngleX = roll;
-  compAngleY = pitch;
-  timer = micros();
-}
-*/
-void computeKalman(){
-  delta = (double)(micros() - timer) / 1000000; // Calculate delta time
-  timer = micros();
-  double gyroXrate = myIMU.gx / 131.0; // Convert to deg/s
-  double gyroYrate = myIMU.gy / 131.0; // Convert to deg/s
-
-#ifdef RESTRICT_PITCH
-  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
-  if ((myIMU.roll < -90 && kalAngleX > 90) || (myIMU.roll > 90 && kalAngleX < -90)) {
-    kalmanX.setAngle(myIMU.roll);
-    compAngleX = myIMU.roll;
-    kalAngleX  = myIMU.roll;
-    gyroXangle = myIMU.roll;
-  } else
-    kalAngleX = kalmanX.getAngle(myIMU.roll, gyroXrate, delta); // Calculate the angle using a Kalman filter
-
-  if (abs(kalAngleX) > 90)
-    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
-  kalAngleY = kalmanY.getAngle(myIMU.pitch, gyroYrate, delta);
-#else
-  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
-  if ((myIMU.pitch < -90 && kalAngleY > 90) || (myIMU.pitch > 90 && kalAngleY < -90)) {
-    kalmanY.setAngle(myIMU.pitch);
-    compAngleY = myIMU.pitch;
-    kalAngleY = myIMU.pitch;
-    gyroYangle = myIMU.pitch;
-  } else
-    kalAngleY = kalmanY.getAngle(myIMU.pitch, gyroYrate, delta); // Calculate the angle using a Kalman filter
-
-  if (abs(kalAngleY) > 90)
-    gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
-  kalAngleX = kalmanX.getAngle(myIMU.roll, gyroXrate, delta); // Calculate the angle using a Kalman filter
-#endif
-
-  gyroXangle += gyroXrate * delta; // Calculate gyro angle without any filter
-  gyroYangle += gyroYrate * delta;
-  //gyroXangle += kalmanX.getRate() * delta; // Calculate gyro angle using the unbiased rate
-  //gyroYangle += kalmanY.getRate() * delta;
-
-  compAngleX = 0.93 * (compAngleX + gyroXrate * delta) + 0.07 * myIMU.roll; // Calculate the angle using a Complimentary filter
-  compAngleY = 0.93 * (compAngleY + gyroYrate * delta) + 0.07 * myIMU.pitch;
-
-  // Reset the gyro angle when it has drifted too much
-  if (gyroXangle < -180 || gyroXangle > 180)
-    gyroXangle = kalAngleX;
-  if (gyroYangle < -180 || gyroYangle > 180)
-    gyroYangle = kalAngleY;
-
-}
-void printKalman(){
-  Serial.print("roll="); Serial.print(myIMU.roll); Serial.print("\t");
-  Serial.print("  gx=");Serial.print(myIMU.gx); Serial.print("\t");
-  Serial.print("gxagl=");Serial.print(gyroXangle); Serial.print("\t");
-  Serial.print("caglx=");Serial.print(compAngleX); Serial.print("\t");
-  Serial.print("kaglx=");Serial.print(kalAngleX); Serial.print("\t");
-  Serial.print("pitch=");Serial.print(myIMU.pitch); Serial.print("\t");
-  Serial.print("  gy=");Serial.print(myIMU.gy); Serial.print("\t");
-  Serial.print("gyagl=");Serial.print(gyroYangle); Serial.print("\t");
-  Serial.print("cagly=");Serial.print(compAngleY); Serial.print("\t");
-  Serial.print("kagly=");Serial.print(kalAngleY); Serial.println("\t");
-
-}
 //////////////////////////////////////////////////////////////////////////////////
 void setup() {
   Wire.begin();
   Serial.begin(BT_RATE);
   BLE_Serial.begin(BT_RATE);
   initEscs();
+#ifdef MPU_9250
   initMPU9250();
+  getDataMPU9250();
+#else
+  initMPU6050();
+  getDataMPU6050();
+#endif
+  initKalman();
   //writeTo4Escs(33);
 }
 void loop() {
   readCmd();
+#ifdef MPU_9250
   getDataMPU9250();
   //printMPU9250();
+#else
+  getDataMPU6050();
+  //printMPU6050();
+#endif
   computeKalman();
-  //printKalman();
-  autoBalance();
+  printKalman();
+  //autoBalance();
   delay(200);
 }
 void readCmd(){
@@ -147,7 +75,7 @@ void readCmd(){
       else cmd+=s;
     }
     if (cmd_end==true){
-      new_speed=cmd.toInt();
+      int new_speed=cmd.toInt();
       //Serial.println("set avg speed to: "+cmd+" avg="+String(avg_speed));
       if(new_speed<50){
         avg_speed=changeAllSpeed(new_speed-avg_speed);
@@ -162,21 +90,42 @@ void readCmd(){
 int changeAllSpeed(int step){
   tmp_speed_total=0;
   for (i=0;i<ArraySize(escs);i++){
-    tmp_speed=escs[i].read();
+    tmp_speed=escs[i].read();  //readMicroseconds
     tmp_speed+=step;
     escs[i].write(tmp_speed);
-    tmp_speed_total+=escs[i].read();
+    //escs[id].writeMicroseconds(tmp_speed);
+    tmp_speed_total+=escs[i].read();  //readMicroseconds
     Serial.print("change speed:"+tmp_speed);
   }
   return tmp_speed_total/ArraySize(escs);
 }
 
 int changeASpeed(int id, int step){
-  tmp_speed=escs[id].read();
+  tmp_speed=escs[id].read();  //readMicroseconds
   tmp_speed+=step;
-  escs[id].write(tmp_speed);
+  escs[id].write(tmp_speed);  //writeMicroseconds
   tmp_speed_total+=step;
   return tmp_speed_total/ArraySize(escs);
+}
+void initMPU6050(){
+  byte c = myIMU.readByte(MPU6050_ADDRESS, WHO_AM_I_MPU6050);
+  //Serial.print("WHO_AM_I="+String(WHO_AM_I)+ "  c="+String(c));
+  if (c == WHO_AM_I){
+    myIMU.MPU6050SelfTest(myIMU.selfTest);
+    myIMU.calibrateMPU6050(myIMU.gyroBias, myIMU.accelBias);
+    myIMU.initMPU6050();
+    byte d = myIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
+    if (d != 0x48) Error(d," WHO_AM_I_AK8963 should be 0x48");
+    myIMU.initAK8963(myIMU.factoryMagCalibration);
+      myIMU.getAres();
+      myIMU.getGres();
+      myIMU.getMres();
+      myIMU.magCalMPU6050(myIMU.magBias, myIMU.magScale);
+    delay(2000);
+    Serial.print("MPU6050 ready");
+  }else{
+    Error(c,"Could not connect to MPU6050: 0x");
+  }
 }
 void initMPU9250(){
   byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
@@ -198,6 +147,7 @@ void initMPU9250(){
     Error(c,"Could not connect to MPU9250: 0x");
   }
 }
+void getDataMPU6050(){}
 void getDataMPU9250(){
   if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01) {
         myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
@@ -322,4 +272,77 @@ int Error(int code, String msg){
   //Serial.println(F("Communication failed, abort!"));
   Serial.flush();
   abort();
+}
+//////////////////////////////////////////////////////////////////////////////////
+
+void initKalman(){
+  kalmanX.setAngle(myIMU.roll); // Set starting angle
+  kalmanY.setAngle(myIMU.pitch);
+  gyroXangle = myIMU.roll;
+  gyroYangle = myIMU.pitch;
+  compAngleX = myIMU.roll;
+  compAngleY = myIMU.pitch;
+  timer = micros();
+}
+
+void computeKalman(){
+  delta = (double)(micros() - timer) / 1000000; // Calculate delta time
+  timer = micros();
+  double gyroXrate = myIMU.gx / 131.0; // Convert to deg/s
+  double gyroYrate = myIMU.gy / 131.0; // Convert to deg/s
+
+#ifdef RESTRICT_PITCH
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((myIMU.roll < -90 && kalAngleX > 90) || (myIMU.roll > 90 && kalAngleX < -90)) {
+    kalmanX.setAngle(myIMU.roll);
+    compAngleX = myIMU.roll;
+    kalAngleX  = myIMU.roll;
+    gyroXangle = myIMU.roll;
+  } else {
+    kalAngleX = kalmanX.getAngle(myIMU.roll, gyroXrate, delta); // Calculate the angle using a Kalman filter
+  }
+  if (abs(kalAngleX) > 90)
+    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleY = kalmanY.getAngle(myIMU.pitch, gyroYrate, delta);
+#else
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((myIMU.pitch < -90 && kalAngleY > 90) || (myIMU.pitch > 90 && kalAngleY < -90)) {
+    kalmanY.setAngle(myIMU.pitch);
+    compAngleY = myIMU.pitch;
+    kalAngleY = myIMU.pitch;
+    gyroYangle = myIMU.pitch;
+  } else {
+    kalAngleY = kalmanY.getAngle(myIMU.pitch, gyroYrate, delta); // Calculate the angle using a Kalman filter
+  }
+  if (abs(kalAngleY) > 90)
+    gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleX = kalmanX.getAngle(myIMU.roll, gyroXrate, delta); // Calculate the angle using a Kalman filter
+#endif
+
+  gyroXangle += gyroXrate * delta; // Calculate gyro angle without any filter
+  gyroYangle += gyroYrate * delta;
+  //gyroXangle += kalmanX.getRate() * delta; // Calculate gyro angle using the unbiased rate
+  //gyroYangle += kalmanY.getRate() * delta;
+
+  compAngleX = 0.93 * (compAngleX + gyroXrate * delta) + 0.07 * myIMU.roll; // Calculate the angle using a Complimentary filter
+  compAngleY = 0.93 * (compAngleY + gyroYrate * delta) + 0.07 * myIMU.pitch;
+
+  // Reset the gyro angle when it has drifted too much
+  if (gyroXangle < -180 || gyroXangle > 180)
+    gyroXangle = kalAngleX;
+  if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+
+}
+void printKalman(){
+  Serial.print("roll="); Serial.print(myIMU.roll); Serial.print("\t");
+  Serial.print("  gx=");Serial.print(myIMU.gx); Serial.print("\t");
+  Serial.print("gxagl=");Serial.print(gyroXangle); Serial.print("\t");
+  Serial.print("caglx=");Serial.print(compAngleX); Serial.print("\t");
+  Serial.print("kaglx=");Serial.print(kalAngleX); Serial.print("\t");
+  Serial.print("pitch=");Serial.print(myIMU.pitch); Serial.print("\t");
+  Serial.print("  gy=");Serial.print(myIMU.gy); Serial.print("\t");
+  Serial.print("gyagl=");Serial.print(gyroYangle); Serial.print("\t");
+  Serial.print("cagly=");Serial.print(compAngleY); Serial.print("\t");
+  Serial.print("kagly=");Serial.print(kalAngleY); Serial.println("\t");
 }
